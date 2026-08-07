9b7241289b72b1a8d87ca04c888be566d344707d
sophieblousson <sophieblousson@gmail.com>
2026-08-07 00:54:56 -0300

    Update page.tsx

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,24 +1,18 @@
 import Hero from "@/components/home/Hero";
-import QueEs from "@/components/home/QueEs";
-import Tema2026 from "@/components/home/Tema2026";
 import Categorias from "@/components/home/Categorias";
 import ComoParticipar from "@/components/home/ComoParticipar";
-import FechasImportantes from "@/components/home/FechasImportantes";
-import CriteriosEvaluacion from "@/components/home/CriteriosEvaluacion";
-import OriginalidadIA from "@/components/home/OriginalidadIA";
+import Premios from "@/components/home/Premios";
+import TemasPorCategoria from "@/components/home/TemasPorCategoria";
 import CTAFinal from "@/components/home/CTAFinal";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
-      <QueEs />
-      <Tema2026 />
       <Categorias />
       <ComoParticipar />
-      <FechasImportantes />
-      <CriteriosEvaluacion />
-      <OriginalidadIA />
+      <Premios />
+      <TemasPorCategoria />
       <CTAFinal />
     </>
   );
