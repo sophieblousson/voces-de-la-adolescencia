96ef507cad5fcd81c6c4b3b89811f17c86eaeaaf
sophieblousson <sophieblousson@gmail.com>
2026-08-07 13:56:24 -0300

    Add Premios component to HomePage

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/home/Hero";
 import Categorias from "@/components/home/Categorias";
 import ComoParticipar from "@/components/home/ComoParticipar";
+import Premios from "@/components/home/Premios";
 import CTAFinal from "@/components/home/CTAFinal";
 
 export default function HomePage() {
@@ -9,6 +10,7 @@ export default function HomePage() {
       <Hero />
       <Categorias />
       <ComoParticipar />
+      <Premios />
       <CTAFinal />
     </>
   );
